c1b535e52a2b9ea690f7d34eeaa6af7927ba31f6
madhu-kuncham <kmadhukumar88011@gmail.com>
2020-08-16 17:52:10 +0530

    modal changes

--- task/src/Components/Modal/usersModal.js
+++ task/src/Components/Modal/usersModal.js
@@ -7,11 +7,12 @@ export const usersModal = ({ openUserDetails, closeUserModal, selectedUser }) =>
         <div className='modal-wrapper'>
             <Modal className='usermodal' isOpen={openUserDetails}>
                 <div className='userdata'>
-                    {selectedUser.email}
-                    {selectedUser.name}
-                    {selectedUser.first_name}
-                    {selectedUser.last_name}
-                    <img src={selectedUser.avatar} width="45" height="35" />
+                    <ul>
+                        <li classname='word-spacing'>{selectedUser.email}</li>
+                        <li className='word-spacing'>{selectedUser.first_name} {selectedUser.last_name}</li>
+                        <li className='word-spacing'><img src={selectedUser.avatar} width="45" height="35" /></li>
+                    </ul>
+
                 </div>
                 <button className='close-button' onClick={(e) => closeUserModal(false)} >Close</button>
             </Modal>
